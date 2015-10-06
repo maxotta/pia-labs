5041f0bb6c485c0615e9ae646051bc2021594b93
Jakub Danek <danek.ja@gmail.com>
2015-10-06 11:40:47 +0200

    [jpa-main-solved] final result of JPA labs.

---
+++ jpa-main-solved/src/main/java/org/danekja/edu/pia/App.java
@@ -0,0 +1,32 @@
+package org.danekja.edu.pia;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import org.danekja.edu.pia.dao.jpa.RoleDaoJpa;
+import org.danekja.edu.pia.dao.jpa.UserDaoJpa;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+
+    private static final String PERSISTENCE_UNIT = "org.danekja.edu.pia";
+
+    public static void main( String[] args ) {
+        //init
+        EntityManagerFactory factory = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT);
+        EntityManager em = factory.createEntityManager();
+
+        JpaExamples examples = new JpaExamples(em, new UserDaoJpa(em), new RoleDaoJpa(em));
+
+        //run example methods
+        examples.tryLazySucc();
+
+        //finish up
+        em.close();
+    }
+
+}
---
+++ jpa-main-solved/src/main/java/org/danekja/edu/pia/domain/Address.java
@@ -0,0 +1,67 @@
+package org.danekja.edu.pia.domain;
+
+import javax.persistence.Entity;
+
+/**
+ * Date: 29.9.15
+ *
+ * @author Jakub Danek
+ */
+@Entity
+public class Address extends BaseEntity<Long> {
+
+    private String street;
+    private String city;
+
+    public Address() {
+    }
+
+    public Address(String street, String city) {
+        this.street = street;
+        this.city = city;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("Address{");
+        sb.append("street='").append(street).append('\'');
+        sb.append(", city='").append(city).append('\'');
+        sb.append('}');
+        return sb.toString();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Address)) return false;
+
+        Address address = (Address) o;
+
+        if (street != null ? !street.equals(address.street) : address.street != null) return false;
+        return !(city != null ? !city.equals(address.city) : address.city != null);
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = street != null ? street.hashCode() : 0;
+        result = 31 * result + (city != null ? city.hashCode() : 0);
+        return result;
+    }
+}
