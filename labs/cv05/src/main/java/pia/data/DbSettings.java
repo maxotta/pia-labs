27afa38bea59ce43dd3f6225f81384a272d10824
Jakub Danek <danek.ja@gmail.com>
2016-10-18 12:48:17 +0200

    [cv05] for 2016/2017

---
+++ labs/cv05/src/main/java/pia/data/DbSettings.java
@@ -0,0 +1,24 @@
+package pia.data;
+
+import java.text.MessageFormat;
+
+public class DbSettings {
+	private static final String DB_ENCODING = "UTF-8";
+	private String server;
+	private int port;
+	private String user;
+	private String password;
+	private String database;
+
+	public DbSettings(String server, int port, String user, String password, String database) {
+		this.server = server;
+		this.port = port;
+		this.user = user;
+		this.password = password;
+		this.database = database;
+	}
+
+	public String getConnectionUrl() {
+		return MessageFormat.format("jdbc:mysql://{0}:{1,number,#}/{2}?characterEncoding={3}", this.server, this.port, this.database, DB_ENCODING);
+	}
+}
